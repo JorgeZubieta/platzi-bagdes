ff6b362bd805dc78a134546eaf238de535e50f3f
jorge zubieta <zubietajor@gmail.com>
2021-06-08 20:34:21 -0300

    jsx example

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-# platzi-badges
-=======
 # platzi-bagdes
->>>>>>> 4362611f0fcbd87cd751b70f10b020afcea8b698
--- src/index.js
+++ src/index.js
@@ -8,9 +8,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+// const jsx = <h1>Hello, Platzi Badges!</h1>;
+// const element = React.createElement(
+//     'a',
+//     { href: 'https://www.platzi.com' },
+//     'Ir a platzi'
+//     );
+const name = ' Jorge Zubieta';
+// const element = React.createElement('h1',{},`Mi nombre es ${name}`)
+// las comillas del tercer argumento debenn ser diagonal ivertidas y el simbolo de $!
+
+
+// Ahora lo hacemos sin jsx lo cual es mas trabajoso y no expresivo ni calro!!!
+// const element = React.createElement(
+//     'div',
+//     {},
+//     React.createElement('h1', {},`Hola soy Kaito`),
+//     React.createElement('p', {}, `Soy hijo de Jorge Zubieta`)
+// );
+
+// ahora lo hacemos con Jsx
+const jsx = (
+    <div>
+        <h1>Hola soy, {name}</h1>
+        <p>Soy Frontend Developer</p>
+    </div>
+)
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(jsx, container);
