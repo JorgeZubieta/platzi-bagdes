cf1856b474eb5bb938f6238d61e58c1c887bbcd9
jorge zubieta <zubietajor@gmail.com>
2021-06-09 20:48:49 -0300

    img and css added

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
+import conflogo from '../images/badge-header.svg';
+
 // las clases son componentens entonces la  clase badge extiende a la clase react.component
 class Badge extends React.Component {
     render() {
-        return <h1>Badge!!!!</h1>;
+        return <div>
+            <div>
+                <img src ={conflogo} alt="logo de la conferencia"/>
+            </div>
+
+            <div>
+                <img src="https://secure.gravatar.com/avatar/e369444fbda201a6d3fca939daec8ed8" alt="Gravatar"/>
+                <h1>Jorge <br/> Zubieta</h1>
+            </div>
+
+            <div>
+                <p>Frontend Developer</p>
+                <p>@jorgezubieta</p>
+            </div>
+
+            <div>
+                #platziconf
+            </div>
+        </div>
     } 
 }
 // Como etamos en un arochivo debemos exportar este componente!
