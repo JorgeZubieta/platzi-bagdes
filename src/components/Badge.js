183dc0b6a456c2f4d77e3881fcc7138938af98e0
jorge zubieta <zubietajor@gmail.com>
2021-06-09 18:35:07 -0300

    component Badge added

---
+++ src/components/Badge.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// las clases son componentens entonces la  clase badge extiende a la clase react.component
+class Badge extends React.Component {
+    render() {
+        return <h1>Badge!!!!</h1>;
+    } 
+}
+// Como etamos en un arochivo debemos exportar este componente!
+export default Badge; 
--- src/index.js
+++ src/index.js
@@ -1,41 +1,9 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
-
-// const container = document.getElementById('app');
-
-// container.appendChild(element);
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// const jsx = <h1>Hello, Platzi Badges!</h1>;
-// const element = React.createElement(
-//     'a',
-//     { href: 'https://www.platzi.com' },
-//     'Ir a platzi'
-//     );
-const name = ' Jorge Zubieta';
-// const element = React.createElement('h1',{},`Mi nombre es ${name}`)
-// las comillas del tercer argumento debenn ser diagonal ivertidas y el simbolo de $!
-
-
-// Ahora lo hacemos sin jsx lo cual es mas trabajoso y no expresivo ni calro!!!
-// const element = React.createElement(
-//     'div',
-//     {},
-//     React.createElement('h1', {},`Hola soy Kaito`),
-//     React.createElement('p', {}, `Soy hijo de Jorge Zubieta`)
-// );
-
-// ahora lo hacemos con Jsx
-const jsx = (
-    <div>
-        <h1>Hola soy, {name}</h1>
-        <p>Soy Frontend Developer</p>
-    </div>
-)
+import Badge from './components/Badge';
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(jsx, container);
+ReactDOM.render(<Badge />, container);
