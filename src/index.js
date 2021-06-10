754a8e23655172f3c7e16086de99d6273ad4b3cf
jorge zubieta <zubietajor@gmail.com>
2021-06-10 00:08:01 -0300

    Props in badge and index added

--- src/index.js
+++ src/index.js
@@ -11,4 +11,12 @@ import "./global.css";
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(<Badge />, container);
+ReactDOM.render(<Badge
+                    firstName="Jorge"
+                    lastName="Zubieta"
+                    avatarUrl="https://secure.gravatar.com/avatar/e369444fbda201a6d3fca939daec8ed8"
+                    jobTitle="Frontend Developer"
+                    // no agreggo el @ en Jorge Zubieta!
+                    twitter="jorgezubieta"
+                    />,
+                    container);
